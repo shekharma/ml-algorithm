b196571cc7627406db30435d8f8d9271df51dd26
Shekhar Mohite <shekharmohite006@gmail.com>
2026-04-15 18:04:29 +0530

    logistic regression with vectorized computations

--- logistic_regression.py
+++ logistic_regression.py
@@ -50,3 +50,38 @@ for epoch in range(epochs):
         print(f"Epoch {epoch}, Loss: {total_loss:.4f}, W: {w:.4f}, b: {b:.4f}")
 
 print("\nFinal:", w, b)
+
+print("###################################################################################")
+# Let's right using numpy- all operation at once and not the pointwise
+import numpy as np
+x = np.array([0, 3, 7, 8, 9])
+y = np.array([0, 0, 1, 1, 1])
+
+
+lr = 0.01
+epochs = 1000
+n = len(x)
+
+epsilon = 1e-9
+
+for epoch in range(epochs):
+    dw = 0
+    db = 0
+    total_loss = 0
+
+    z = w*x + b
+    y_hat = 1/ (1+np.exp(-z))
+    y_hat = np.clip(epsilon,epsilon, 1 - epsilon)
+
+    loss = np.mean(-(y*np.log(y_hat)+ (1-y)*np.log(1-y_hat)))
+
+    dw = np.mean((y_hat - y)*x)
+    db = np.mean(y_hat - y)
+    # update
+    w -= lr * dw
+    b -= lr * db
+
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}, Loss: {loss:.4f}")
+
+print("Final:", w, b)
