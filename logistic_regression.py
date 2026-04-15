4571fefbf4d4b3a98ac6f247c5bacbe0013206c3
Shekhar Mohite <shekharmohite006@gmail.com>
2026-04-15 17:12:39 +0530

    logistic regression

---
+++ logistic_regression.py
@@ -0,0 +1,52 @@
+# logistic regression
+# sigmoid function = y_hat= f(x) = 1/(1+e^-z)  and z = -wx+b
+# log-loss = -(y*log(y_hat)+ (1-y)*log(1-y_hat))
+# dl/dw = (y_hat - y)*x
+# dl/db = (y_hat - y)
+
+import random
+import math
+
+w = random.uniform(-1, 1)
+b = random.uniform(-1, 1)
+
+x = [0, 3, 7, 8, 9]
+y = [0, 0, 1, 1, 1]
+
+lr = 0.01
+epochs = 1000
+n = len(x)
+
+epsilon = 1e-9 ## addition of this is necessary beacuse log(0) is not defined y_hat =0 or y_hat=1
+
+for epoch in range(epochs):
+    dw = 0
+    db = 0
+    total_loss = 0
+
+    for i in range(n):
+        z = w * x[i] + b
+        y_hat = 1 / (1 + math.exp(-z))
+
+        # clipping
+        y_hat = max(epsilon, min(1 - epsilon, y_hat))
+
+        loss = -(y[i]*math.log(y_hat) + (1-y[i])*math.log(1-y_hat))
+        total_loss += loss
+
+        dw += (y_hat - y[i]) * x[i]
+        db += (y_hat - y[i])
+
+    # average
+    dw /= n
+    db /= n
+    total_loss /= n
+
+    # update
+    w -= lr * dw
+    b -= lr * db
+
+    if epoch % 100 == 0:
+        print(f"Epoch {epoch}, Loss: {total_loss:.4f}, W: {w:.4f}, b: {b:.4f}")
+
+print("\nFinal:", w, b)
